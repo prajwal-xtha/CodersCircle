7133f7c5ab6c54416ae71d8b5f1b6e927d57e0fa
prajwal-xtha <prajwalshrestha794@gmail.com>
2026-01-05 14:42:21 +0545

    new responsive

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -2,16 +2,16 @@ function Landing() {
   return (
     <>
       <div className="items-center flex justify-center h-screen w-screen bg-gray-950 text-white">
-        <video autoPlay muted loop playsInline className="absolute top-0 left-0 w-full h-full object-cover z-0 blur-sm">
+        {/* <video autoPlay muted loop playsInline className="absolute top-0 left-0 w-full h-full object-cover z-0 blur-sm">
   <source src="../../public/background.mp4" type="video/mp4" />
   Your browser does not support the video tag.
-</video>
+</video> */}
       {/* <video autoPlay muted loop playsInline className="absolute top-0 left-0 w-full h-full object-cover">
   <source src="/your-video.mp4" type="video/mp4" />
   Your browser does not support the video tag.
 </video> */}
 <div className="z-1 flex flex-col items-center ">
-<p className="flex justify-center items-center h-[40px] w-[290px] mb-6 rounded-3xl border border-blue-200 bg-transparent font-mono text-[15px]">
+<p className="flex justify-center items-center sm:h-[40px] sm:w-[290px] mb-6  h-[40px] w-[255px] rounded-3xl border border-blue-200 bg-transparent font-mono sm:text-[15px] text-[13px]">
   <span className="text-blue-200">
     &gt;_&nbsp;&nbsp;&gt;
   </span>
@@ -19,23 +19,23 @@ function Landing() {
 </p>
 
 
-        <h1 className="text-7xl font-bold text-center">
+        <h1 className="sm:text-7xl text-4xl font-bold text-center">
           Where  Code  Meets <br />
           <span
-            className="text-6xl font-extrabold tracking-tight leading-tight
+            className=" sm:text-6xl text-4xl font-extrabold tracking-tight leading-tight
            bg-gradient-to-r from-blue-500 via-sky-200 to-purple-500 
            bg-clip-text text-transparent drop-shadow-[0_0_50px_rgba(0,189,248,0.8)]"
           >
             Community
           </span>
         </h1>
-        <p className="w-[700px] text-center text-[25px] text-gray-500  drop-shadow-[0_0_50px_rgba(0,189,248,0.8)]">
+        <p className="sm:w-[700px] w-[350px] text-center text-[15px] text-gray-500  drop-shadow-[0_0_50px_rgba(0,189,248,0.8)]">
           Join a passionate community of developers, learners, and innovators.
           Build, learn, and grow together at Coding Circle
         </p>
         <div className="pt-8">
           <button
-            className="pr-8  pl-8 pt-3 pb-3 rounded-xl bg-gradient-to-r from-blue-500 via-sky-500 to-purple-500
+            className="sm:pr-8  sm:pl-8 sm:pt-3 sm:pb-3 pr-5 pl-5 pb-3 pt-3 rounded-xl bg-gradient-to-r from-blue-500 via-sky-500 to-purple-500
                text-black font-bold shadow-lg
                cursor-pointer
                drop-shadow-[0_0_50px_rgba(0,189,248,0.8)]
@@ -43,7 +43,7 @@ function Landing() {
           >
             Join the circle  ➔
           </button>
-          <button className="pr-8 ml-5 pl-8 pt-3 pb-3 cursor-pointer rounded-xl bg-transparent text-white border border-blue-300">
+          <button className="sm:pr-8  sm:pl-8 sm:pt-3 sm:pb-3 pr-5 pl-5 pb-3 pt-3 rounded-xl ml-6 cursor-pointer rounded-xl bg-transparent text-white border border-blue-300">
             Learn more
           </button>
         </div>
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,16 +1,21 @@
+
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faHome } from "@fortawesome/free-solid-svg-icons";
+import { faCake } from "@fortawesome/free-solid-svg-icons/faCake";
+
 
 const Navbar = () => {
   return(
     <>
-    <nav class="fixed top-4 left-1/2 -translate-x-1/2 
+    <nav class="sm:fixed top-4 left-1/2 -translate-x-1/2 
             w-[90%] max-w-xl 
             bg-black/20 backdrop-blur-xl 
             border border-white/20 
             shadow-lg 
             rounded-2xl 
-            px-6 py-3 
-            flex items-center justify-center 
+            px-6 py-3 flex hidden
+            sm:flex items-center justify-center 
             z-50 opacity-105 font-bold">
 
   {/* <h1 class="text-white font-semibold text-lg">
@@ -23,7 +28,12 @@ const Navbar = () => {
     <li class="hover:text-white transition">CONTACT US</li>
   </ul>
 </nav>
-
+<nav className=" text-white sm:hidden flex w-screen h-15 bg-gray-950  text-center items-center justify-between border-b border-white">
+  <h1 className="ml-5"><img src="public/CodersCircleLogo.png" className= "h-10 w-10 rounded-3xl"/></h1>
+  <div className="text-2xl mr-4">
+    <FontAwesomeIcon icon={faBars} />
+  </div>
+</nav>
     </>
   )
 };
